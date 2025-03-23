5fdd73cba9fe9f00629fec0a828c5bb83b11f9b5
Barbora6 <smetanovabarbora@email.cz>
2025-03-23 07:53:36 +0100

    dokonceni hry

--- hra_Breakout/index.js
+++ hra_Breakout/index.js
@@ -1,9 +1,25 @@
+// hra Breakout
+// Breakout je jednoduchá flashová vezrze hry Arkanoid. Posouváte plošinkou, a tím odrážíte míček, který nabourává strukturu barevných cihliček. Jakmile vám míček propadne, ztrácíte jeden život.
+
+// https://www.youtube.com/watch?v=ec8vSKJuZTk&t=5445s
+
 const grid = document.querySelector(".grid");
-const blockWidth = 100;
+const scoreDisplay = document.querySelector("#score");
+const blockWidth = 80;
 const blockHeight = 20;
+const ballDiameter = 20;
+const boardWidth = 560;
+const boardHeight = 300;
+let timerId;
+let xDirection = -2;
+let yDirection = 2;
+let score = 0;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
+
+const ballStart = [270, 40];
+let ballCurrentPosition = ballStart;
 
 // create Block
 /*
@@ -75,6 +91,12 @@ function drawUser() {
   user.style.bottom = currentPosition[1] + "px";
 }
 
+// draw the ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
+}
+
 // move user
 function moveUser(e) {
   switch (e.key) {
@@ -83,7 +105,12 @@ function moveUser(e) {
         currentPosition[0] -= 10;
         drawUser();
       }
-
+      break;
+    case "ArrowRight":
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
       break;
   }
 }
@@ -101,3 +128,92 @@ Naslouchá události "keydown" → tedy stisknutí klávesy.
 Když uživatel stiskne klávesu, zavolá se moveUser(e).
 Parametr e (event) obsahuje informace o stisknuté klávese.
 */
+
+// add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+drawBall();
+grid.appendChild(ball);
+
+// move ball
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+
+timerId = setInterval(moveBall, 25);
+
+// check for collisions
+function checkForCollisions() {
+  // check for block collisions
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPosition[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+      score++;
+      scoreDisplay.innerHTML = score;
+
+      // check for win
+      if (blocks.length === 0) {
+        scoreDisplay.innerHTML = "Vyhrál jsi!!!";
+        clearInterval(timerId);
+        document.removeEventListener("keydown", moveUser);
+      }
+    }
+  }
+
+  // check for wall collisions
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  // check for user collisions
+  if (
+    ballCurrentPosition[0] > currentPosition[0] &&
+    ballCurrentPosition[0] < currentPosition[0] + blockWidth &&
+    ballCurrentPosition[1] > currentPosition[1] &&
+    ballCurrentPosition[1] < currentPosition[1] + blockHeight
+  ) {
+    changeDirection();
+  }
+
+  // check for game over
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "Prohrál jsi.";
+    document.removeEventListener("keydown", moveUser);
+  }
+}
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection == 2 && yDirection == -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection == -2 && yDirection == -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
+}
